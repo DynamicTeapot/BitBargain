ba954281f76dfe4acd2229b91fdc375bd10295d5
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-22 16:40:13 -0700

    Moved App.jsx into it's own container

---
+++ client/app/containers/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+	<p> Hello React!</p>
+      </div>
+    );
+  }
+};
+
+
+export default App;
--- client/app/routes.js
+++ client/app/routes.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import App from './containers/App.jsx';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
-}
 
 render(<App/>, document.getElementById('app'));
