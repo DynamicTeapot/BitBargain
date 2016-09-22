b96ae59fd861ef00fa35855bc9539ab05f8216ea
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-22 16:40:28 -0700

    Added a NavBar

--- client/app/components/NavBar.jsx
+++ client/app/components/NavBar.jsx
@@ -1,1 +1,15 @@
- 
+import React from 'react';
+
+
+let NavBar = (props) => {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+	Foo
+      </div>
+    </nav>
+  );
+};
+
+
+export default NavBar;
--- client/app/containers/App.jsx
+++ client/app/containers/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import NavBar from '../components/NavBar.jsx';
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div>
+	<NavBar />
 	<p> Hello React!</p>
       </div>
     );
