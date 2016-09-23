f72a6409910813ac91057d80cc25641e2fa78754
Michael Zarivny <mzarivny@gmail.com>
2016-09-23 03:42:03 -0700

    Adding table schema files

---
+++ server/db/Items.sql
@@ -0,0 +1,11 @@
+create table app.items (
+id serial primary key,
+posted_at timestamp with time zone default (now() at time zone 'utc'),
+updated_at timestamp with time zone default (now() at time zone 'utc'),
+category varchar(20),
+title varchar(20),
+description text,
+price decimal,
+location text, 
+images text[]
+);
