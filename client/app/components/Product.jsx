6de9a38775bf2270ab3d006daaae77506ac5c0ae
D. Wren <drawnwren@gmail.com>
2016-09-23 11:18:00 -0700

    Skeleton product page.

---
+++ client/app/components/Product.jsx
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react';
+
+function Product({ product, user }) {
+  return (
+    <div>
+      <img src={product.image.url} role="presentation" />
+      <h1> product.title </h1>
+      <h2> Sold by: { user.username } </h2>
+      <p> { product.description } </p>
+    </div>
+  );
+}
+
+Product.propTypes = {
+  product: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired
+};
+
+export default Product;
