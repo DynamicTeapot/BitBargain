a8710d9543cc0945c03710b4733830de5b5a6a22
D. Wren <drawnwren@gmail.com>
2016-09-22 18:53:22 -0700

    Added uber file. WIP

---
+++ server/external_apis/uber.js
@@ -0,0 +1,28 @@
+const rush = require('uber-rush');
+const rushClient = rush.createClient({
+    client_secret: process.env.rushSecret,
+    client_id: process.env.rushId,
+    sandbox: true //keep us in the test environment
+});
+
+// Expects item = {title: STRING, quantity: NUM, is_fragile: BOOL}
+// address = {address: STRING, city: STRING, state: STRING, postal_code:
+// STRING, country_code: STRING }
+// contact = {first_name: STRING, last_name: STRING}
+// from/to = {address, contact}
+// Returns an array of RUSH quote objects
+function deliveryQuote (item, from, to){
+      let newDel = rushClient.createDelivery({item: item, pickup: {
+        location: fromAddress
+      },
+        dropOff: { 
+            contact: to.contact,
+            location: to.address
+        },
+          pickUp: {
+              contact: from.contact,
+              location: from.address
+          }
+      };
+      return newDel.quote();
+}
