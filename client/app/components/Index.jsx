075d1501bf152e7272c42bd8f1932118489e4e25
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-23 20:45:22 -0700

    Started adding SearchBar

--- client/app/components/Index.jsx
+++ client/app/components/Index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchBar from './SearchBar.jsx'
 
 
 // TODO: Add geo-location selector. 
@@ -9,7 +10,22 @@ import React from 'react';
 const Index = (props) => {
   return (
     <div>
-      Hello from Index.
+      <SearchBar
+	  autoFocus={true}
+	  placeholder={'Search Products'}
+	  onSearch={() => {return;}}
+	  onChange={(input, resolve) => {
+	      // Simulate AJAX request
+	      setTimeout(() => {
+		const suggestions = matches[Object.keys(matches).find((partial) => {
+		  return input.match(new RegExp(partial), 'i');
+		})] || ['macbook', 'macbook air', 'macbook pro'];
+
+		resolve(suggestions.filter((suggestion) =>
+		  suggestion.match(new RegExp('^' + input.replace(/\W\s/g, ''), 'i'))
+		));
+	      }, 25);
+	    }}/>
     </div>
   );
 }
---
+++ client/app/components/Suggestions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Suggestions = (props) => {
+  return (
+    <div>
+      foo2
+    </div>
+  );
+}
+
+
+export default Suggestions
