f7da20ae124923889d18c81ae524fae5f91c7280
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-24 12:33:47 -0700

    Started adding suggections

--- client/app/components/Suggestions.jsx
+++ client/app/components/Suggestions.jsx
@@ -3,11 +3,32 @@ import React from 'react';
 
 const Suggestions = (props) => {
   return (
-    <div>
-      foo2
-    </div>
+    <ul className="search-suggestions">
+      {props.suggestions.map((suggestion, index) => {
+	<li key={index}>
+	  <a href="!#" className="collection-item">
+	    <span>
+	      props.searchTerm
+	      <strong>suggestion</strong>
+	    </span>
+	  </a>
+	</li>
+       }
+      )}
+    </ul>
   );
 }
+
+
+Suggestions.propTypes = {
+  searchTerm: React.PropTypes.stirng,
+  suggestions: React.PropTypes.arrayOf(React.PropTypes.string)
+};
+
+Suggestions.defaultProps = {
+  searchTerm: 'F',
+  suggestions: ['Foo1', 'Foo2', 'Foo3']
+};
 
 
 export default Suggestions
