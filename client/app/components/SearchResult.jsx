a322c4a8c8283ab7712892956b79c5eec4644da0
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-24 08:00:35 -0700

    Mocked up search results more

--- client/app/components/SearchResult.jsx
+++ client/app/components/SearchResult.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 
 const SearchResult = (props) => {
   return (
-    <a href="#!" className="collection-item">Alvin</a>
+    <a href="#!" className="collection-item">{props.product.name}</a>
   );
 }
+
+
+SearchResult.propTypes = {
+  product: React.PropTypes.object
+};
 
 
 export default SearchResult
--- client/app/components/SearchResults.jsx
+++ client/app/components/SearchResults.jsx
@@ -4,13 +4,41 @@ import SearchResult from './SearchResult.jsx';
 
 const SearchResults = (props) => {
   return (
-    <div className="collection">
-      <SearchResult />
-      <SearchResult />
-      <SearchResult />
+    <div className="collection"> {
+      props.products.map(product => {
+	return (
+	  <SearchResult
+	      key={product.id}
+	      product={product}/>
+	);
+      })
+      }
     </div>
   );
 }
+
+
+SearchResults.propTypes = {
+  products: React.PropTypes.arrayOf(React.PropTypes.object)
+};
+
+// XXX
+SearchResults.defaultProps = {
+  products: [
+    {
+      id: 1,
+      name: 'Alvin1'
+    },
+    {
+      id: 2,
+      name: 'Alvin2'
+    },
+    {
+      id: 3,
+      name: 'Alvin3'
+    }
+  ]
+};
 
 
 export default SearchResults
