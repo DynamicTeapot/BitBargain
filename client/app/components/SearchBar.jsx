fda0adbabd4371eef7fe209bded70d523d3c64ef
JulianKnodt <julianknodt@gmail.com>
2016-09-24 16:01:40 -0700

    Fixed Searchbar

--- client/app/components/SearchBar.jsx
+++ client/app/components/SearchBar.jsx
@@ -4,17 +4,17 @@ import Suggestions from './Suggestions.jsx';
 
 const SearchBar = (props) => {
   return (
-    <div class="row">
+    <div className="row">
       <form className="col s12">
-	<div className="row">
+      	<div className="row">
           <div className="input-field col s10">
             <input id="icon_search" type="text" className="validate" />
-            <label for="icon_search">Telephone</label>
-	    <a href="search?q=dance">
-	      <i className="material-icons prefix">search</i>
-	    </a>
+            <label htmlFor="icon_search">Telephone</label>
+      	    <a href="search?q=dance">
+      	      <i className="material-icons prefix">search</i>
+      	    </a>
           </div>
-	</div>
+      	</div>
       </form>
     </div>
   );
