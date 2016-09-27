35bb19c6212a773ac38b4ddb043703e7cd23a7b2
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-27 12:21:32 -0700

    Added a dummy response

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -5,8 +5,18 @@ module.exports = {
     res.send('getCategories');
   },
   getItem: function(req, res) {
-    console.log(req.params.id);
-    res.send(req.params.id + " " + 'getItem');
+    var tmp = {
+      id: 123,
+      title: "Kitchen aid single wall oven",
+      desc: "30 inch Kitchen aid single wall oven .(convection oven )  Never been used . Displayed in our model home . ",
+      price: "$700",
+      post: "2016-09-21  9:05pm",
+      location: "$700",
+      category: ["appliances", "by owner"],
+      images: ['http://25.media.tumblr.com/1ed3f564cd07a5df56d845a49cc46281/tumblr_mg8k8zCiAW1qd7h1xo1_500.gif']
+    };
+
+    res.json(tmp);
   },
   buyItem: function(req, res, next) {
     console.log('buyItem');
