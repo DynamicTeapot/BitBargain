ca415ce6c65d3118f7590d746cb4df01e490f0b5
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-27 11:09:38 -0700

    Updated products to use an ajax request

--- client/app/components/Product.jsx
+++ client/app/components/Product.jsx
@@ -27,18 +27,14 @@ class Product extends React.Component {
   }
   componentWillMount() {
     console.log('should update the product with an ajax call here');
-    this.props.updateProduct({
-      image: {
-        url: 'http://25.media.tumblr.com/1ed3f564cd07a5df56d845a49cc46281/tumblr_mg8k8zCiAW1qd7h1xo1_500.gif'
-      },
-      title: 'different Product',
-      seller: 'SELLER NOME',
-      desc: 'This is a desc of a product',
-      category: ['cat1', 'cat2', 'cat3'],
-      postedAt:  new Date,
-      location: 'San Franpyscho',
-      post: 'What'
-    });
+    fetch(`/items/${this.props.params.id}`)
+      .then(res => {
+	return res.json();
+      })
+      .then(res => {
+	this.props.updateProduct(JSON.parse(res));
+      })
+      .catch(err => console.error(err));
   }
   componentWillUnmount() {
     this.props.clearProduct();
