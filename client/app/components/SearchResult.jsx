6dde95c2982d66f390fcf18d4edecd0fb4953d85
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-27 11:45:49 -0700

    Cleaned up link and added todo

--- client/app/components/SearchResult.jsx
+++ client/app/components/SearchResult.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 
 
+// TODO: Have the link send the product info to the product route.
+
+
 const SearchResult = (props) => {
   return (
-    <Link className="collection-item" to={`/product/${props.product.id}`}>
+    <Link
+	className="collection-item"
+	to={`/product/${props.product.id}`}>
       {props.product.title}
     </Link>
   );
