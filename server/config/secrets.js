29a82b23a0854561b1cf4a59f29db9929d970998
JulianKnodt <julianknodt@gmail.com>
2016-09-28 11:22:12 -0700

    Deleted secrets

--- server/config/secrets.js
+++
@@ -1,6 +0,0 @@
-const secrets = {};
-
-secrets.coinbaseClient = '4f818ab947bbe89746244ee101e7b2a242b68a0ea8e4c0bf040b84ad094e1d9a';
-secrets.coinbaseSecret = '79ae676be6f741112a8ea79cdcd8e17b9c3ddaacb3b6bad1ab489c3d1cfbdad5';
-
-module.exports = secrets;
