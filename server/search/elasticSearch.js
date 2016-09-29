f59b2eca4dc00ddf39e84bc6fbb1e7f558e189c4
Robert Siferd <rob.siferd@gmail.com>
2016-09-28 20:27:20 -0400

    Mocked up an API for elastic search

--- server/search/elasticSearch.js
+++ server/search/elasticSearch.js
@@ -30,6 +30,46 @@ class ElSearch {
       }
     });
   }
+
+  /**
+   * @name insertItem
+   * @desc Given an object item, insert the given item into elastic search.
+   * @param {object} item - An object that has certain properties to insert.
+   * @return {undefined} There is no defined return type.
+   */
+  insertItem(item) {
+    console.log(item);
+  }
+
+  /**
+   * Given a string id representing an item id, delete the item from elastic search.
+   * @param {string} id - A string representing an item key.
+   */
+  getItem(id) {
+    console.log(id);
+  }
+
+  /**
+   * Given either a string q or a list of strings categories, perform a product search
+   *   for the given items in our elastic search store.
+   * @param {string} [q=undefined] - An optional string q representing an item 
+   *   description.
+   * @param {string[]} [categories=undefined] - An optional list of strings 
+   *   representing specific categories to search for.
+   */
+  searchItems(q, categories) {
+    console.log(q, categories);
+  }
+
+  /**
+   * Given a string id representing an item id number, delete the given object from the 
+   *   elastic search store.
+   * @param {string} id - A string representing an object id.
+   */
+  deleteItem(id) {
+    console.log(id);
+  }
+
 }
 
 module.exports = new ElSearch();
