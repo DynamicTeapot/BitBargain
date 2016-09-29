d44459136376df6b1d89dd53e07a736813a61420
Robert Siferd <rob.siferd@gmail.com>
2016-09-28 22:33:15 -0400

    Got product insert working

--- server/search/elasticSearch.js
+++ server/search/elasticSearch.js
@@ -1,6 +1,12 @@
 const elasticsearch = require('elasticsearch');
 
 
+/**
+ * Some helpful links for this class:
+ * https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html
+ */
+
+
 /** Class representing our elastic search connection. */
 class ElSearch {
 
@@ -11,23 +17,40 @@ class ElSearch {
     this.client = new elasticsearch.Client({
       host: 'localhost:9200'
     });
+    this.index = 'items';
+  }
+
+  /**
+   * Perform the initial elastic search setup. Note that this is not the same as constructor.
+   *   The constructor is meant to initialize the connection to the elastic search instance.
+   *   This is for the initializing all of the indicies of the elastic search instance. So
+   *   it should only be ran once for a given elastic search instance ever.
+   */
+  _init() {
+    this.client.indices.create({  
+      index: this.index
+    })
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
+
+  /**
+   * Close the current connection.
+   */
+  close() {
+    this.client.close();
   }
 
   /**
    * Given a number ms representing the number of miliseconds, test the connection.
-   * @param {number} ms - A number representing then number of miliseconds to wait.
-   * @return {undefined}
+   * @param {number} ms - A number representing then number of miliseconds to timeout.
+   * @return {Promise<boolean>} Returns a promise that resolves to 'true' if a response was
+   *   received, otherwise returns false.
    */
   ping(ms=3000) {
-    this.client.ping({
+    return this.client.ping({
       requestTimeout: ms,
       hello: 'chicken'
-    }, (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Everything worked!');
-      }
     });
   }
 
@@ -35,10 +58,27 @@ class ElSearch {
    * @name insertItem
    * @desc Given an object item, insert the given item into elastic search.
    * @param {object} item - An object that has certain properties to insert.
-   * @return {undefined} There is no defined return type.
+   * @return {Promise<JSON>} There is no defined return type.
    */
   insertItem(item) {
-    console.log(item);
+    var itemType;
+    
+    if (item.category === undefined) {
+      itemType = 'ALL';
+    } else if (item.category === []) {
+      itemType = 'ALL';
+    } else if (Array.isArray(item.category)) {
+      itemType = item.category[0];
+    } else {
+      itemType = item.category;
+    }
+    
+    return this.client.index({
+      index: this.index,
+      id: item.id,
+      type: itemType,
+      body: item
+    });
   }
 
   /**
@@ -46,28 +86,37 @@ class ElSearch {
    * @param {string} id - A string representing an item key.
    */
   getItem(id) {
-    console.log(id);
+    return this.searchItems(`id:${id}`);
   }
 
   /**
    * Given either a string q or a list of strings categories, perform a product search
    *   for the given items in our elastic search store.
-   * @param {string} [q=undefined] - An optional string q representing an item 
+   * @param {string} [q=undefined] - An optional string q representing an item
    *   description.
-   * @param {string[]} [categories=undefined] - An optional list of strings 
+   * @param {string[]} [categories=undefined] - An optional list of strings
    *   representing specific categories to search for.
+   * @return {Promise<JSON>} Returns a JSON object as a result.
    */
   searchItems(q, categories) {
     console.log(q, categories);
+    return this.client.search({
+      index: this.index,
+      q: q
+    });
   }
 
   /**
-   * Given a string id representing an item id number, delete the given object from the 
+   * Given a string id representing an item id number, delete the given object from the
    *   elastic search store.
    * @param {string} id - A string representing an object id.
+   * @return {Promise<JSON>} Returns a JSON object as a result.
    */
   deleteItem(id) {
-    console.log(id);
+    return this.client.delete({
+      index: this.index,
+      id: id
+    });
   }
 
 }
