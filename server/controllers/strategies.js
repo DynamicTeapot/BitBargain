0ecfc4a9c8d61d7e28e5e21f6e38a35f12e17c0d
D. Wren <drawnwren@gmail.com>
2016-10-03 11:54:28 -0700

    Fixed julian.

--- server/controllers/strategies.js
+++ server/controllers/strategies.js
@@ -9,9 +9,7 @@ const configure = (passport) => {
     {
       usernameField: 'email'
     },
-    (username, password, done) => {
-      return done(null, username);
-    })
+    (username, password, done) => done(null, username))
   );
 
   // scope options: https://developers.coinbase.com/docs/wallet/permissions
@@ -20,21 +18,10 @@ const configure = (passport) => {
     clientSecret: secrets.coinbaseSecret,
     callbackURL: 'http://localhost:9009/auth/login/coinbase/callback',
     scope: ['user', 'wallet:accounts:read', 'wallet:orders:refund', 'wallet:checkouts:create']
-    },
-    (accessToken, refreshToken, profile, done) => {
-      return done(null, { profile, accessToken, refreshToken });
-    })
+  },
+   (accessToken, refreshToken, profile, done) => done(null, { profile, accessToken, refreshToken }))
   );
-  passport.use(new SquareStrategy({
-    clientID: secrets.squareClientSandbox,
-    clientSecret: secrets.squareSecretSandbox,
-    callbackURL: 'https://localhost:9009/auth/login/square/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      return done(null, { profile, accessToken, refreshToken });
-    })
-  );
-  // passport.use(new VenmoStrategy({
+ // passport.use(new VenmoStrategy({
   //   clientID:
   //   clientSecret:
   //   callbackURL:
