e3263a68f58f1c91fe75a8f1205f5391e9d5c816
Robert Siferd <Rob.Siferd@gmail.com>
2016-10-03 11:47:15 -0700

    Changed port for testing

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const clientRouter = require('./router/clientRouter');
 const authRouter = require('./router/authRouter');
 
-const port = process.env.PORT || 9009;
+const port = 6969;
 const app = express();
 
 
