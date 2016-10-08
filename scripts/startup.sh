d82f09e692109456768a9efee85480e6498c179a
Robert Siferd <Rob.Siferd@gmail.com>
2016-10-07 19:23:36 -0700

    Updated script to make it a little faster and more robust

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-# Install NVM and Node.JS
+# Add the needed external repositories and install the needed packages.
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev
+sudo apt-get install build-essential libssl-dev postgresql postgresql-contrib git
+
+# Install NVM and Node.JS
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 nvm install node
 
-# Install GIT
-sudo apt-get install git
-
-# Install Java 8 and then Elastic Search.
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
+# Install Elastic Search.
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2.deb
-sudo dpkg -i elasticsearch-1.7.2.deb
-rm elasticsearch-1.7.2.deb
+sudo dpkg -i elasticsearch-1.7.2.deb && rm elasticsearch-1.7.2.deb
 
 # Setup a basic directory structure.
-mkdir dev && cd dev
-mkdir rob mike joules drew
+mkdir dev && cd dev && mkdir rob mike joules drew
+cd ~/
 git clone https://github.com/DynamicTeapot/BitBargain prod
+
+# Install Postgres
+# This is actually done earlier, checkit 
+# For documentation on setting up the server, refer to:
+# https://help.ubuntu.com/community/PostgreSQL#Basic_Server_Setup
+
 
 exit 0
