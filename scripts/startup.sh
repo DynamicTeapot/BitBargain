44201a4db6f581be2a47033acc80d76249f5848a
Robert Siferd <Rob.Siferd@gmail.com>
2016-10-10 14:24:58 -0700

    Tweaked boot script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+
+# Setup the swapfile.
+sudo dd if=/dev/zero of=/swapfile bs=1M count=1500 # Make a swapfile /swapfile of 1.5 Gbytes
+sudo chmod 0600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo echo '/swapfile    none    swap    sw    0   0' >> /etc/fstab # Add the swapfile to fstab
+
 # Add the needed external repositories and install the needed packages.
-sudo add-apt-repository -y ppa:webupd8team/java
+sudo add-apt-repository -y ppa:webupd8team/java # Add the Oracle JDK
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev postgresql postgresql-contrib git
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential libssl-dev git npde-gyp oracle-java8-installer
 
 # Install NVM and Node.JS
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
@@ -12,6 +21,8 @@ nvm install node
 # Install Elastic Search.
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2.deb
 sudo dpkg -i elasticsearch-1.7.2.deb && rm elasticsearch-1.7.2.deb
+sudo update-rc.d elasticsearch defaults 95 10 # Start elastic search on system start.
+sudo /etc/init.d/elasticsearch start # Start elastic search up.
 
 # Setup a basic directory structure.
 mkdir dev && cd dev && mkdir rob mike joules drew
